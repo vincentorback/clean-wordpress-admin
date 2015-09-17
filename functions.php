0af6b6452ad2575ca5c9eb3e84c74156aca75a11
Vincent Orback <vorback@gmail.com>
2015-09-17 22:07:12 +0200

    Updated paths with the new named files

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
 
 require get_template_directory() . '/user-capabilities.php';
-require get_template_directory() . '/hide-menu.php';
-require get_template_directory() . '/profile-fields.php';
-require get_template_directory() . '/clean-head.php';
+require get_template_directory() . '/admin-menu.php';
+require get_template_directory() . '/profile.php';
+require get_template_directory() . '/head.php';
+require get_template_directory() . '/dashboard.php';
 require get_template_directory() . '/images.php';
-require get_template_directory() . '/profile-fields.php';
 
+require get_template_directory() . '/essential.php';
 require get_template_directory() . '/extras.php';
