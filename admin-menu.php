64452267e447dcf043ee193f99bdb334e29b1eab
Vincent Orback <vorback@gmail.com>
2015-09-17 22:06:17 +0200

    Renamed admin-menu file and removed role check

---
+++ admin-menu.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Hide admin menu items. Can be both parents and children in
+ * dropdowns. Specify link to parent and link to child.
+ * @return [type] [description]
+ */
+add_action('admin_head', function () {
+  remove_menu_page('edit.php');
+  remove_menu_page('upload.php');
+  remove_menu_page('edit-comments.php');
+  remove_menu_page('tools.php');
+
+  remove_submenu_page('themes.php', 'customize.php');
+  remove_submenu_page('themes.php', 'customize.php');
+
+  remove_submenu_page('edit.php', 'widgets.php');
+
+  remove_submenu_page('options-general.php', 'options-permalink.php');
+  remove_submenu_page('options-general.php', 'options-writing.php');
+  remove_submenu_page('options-general.php', 'options-media.php');
+  remove_submenu_page('options-general.php', 'options-reading.php');
+  remove_submenu_page('options-general.php', 'options-discussion.php');
+});
