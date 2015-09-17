60d400aa772b07210fd8d39a7a5ad15d6e63ffd8
Vincent Orback <vorback@gmail.com>
2015-09-17 22:06:27 +0200

    Added some more extra functions

--- extras.php
+++ extras.php
@@ -12,8 +12,47 @@ function is_page_for_posts() {
 /**
  * Custom admin footer
  */
-function custom_admin_footer() {
+add_filter('admin_footer_text', function () {
   echo '<span id="footer-thankyou">Website by <a href="//vincentorback.se" target="_blank">Vincent Orback</a></span>';
+});
+
+
+/**
+ * Nice debugging replacement to var_dump
+ * @param  [type] $data [description]
+ * @return [type]       [description]
+ */
+function show ($data) {
+  echo '<pre style="box-sizing: border-box; height: 50vh; resize: vertical;
+        outline: 2px solid; background: #fff; font: 13px/1.3 monospace">';
+  print_r($data);
+  echo '</pre>';
 }
 
-add_filter('admin_footer_text', 'custom_admin_footer');
+
+/**
+ * Disable comments
+ */
+add_action( 'init', function () {
+  remove_post_type_support( 'post', 'comments' );
+  remove_post_type_support( 'page', 'comments' );
+});
+
+add_action( 'admin_menu', function () {
+  remove_menu_page( 'edit-comments.php' );
+});
+
+add_action( 'wp_before_admin_bar_render', function () {
+  global $wp_admin_bar;
+  $wp_admin_bar->remove_menu('comments');
+});
+
+
+/**
+ * Custom avatar
+ */
+add_filter( 'avatar_defaults', function ($avatar_defaults) {
+  $my_avatar = get_bloginfo('template_directory') . '/images/my-avatar.gif';
+  $avatar_defaults[$my_avatar] = "My Avatar";
+  return $avatar_defaults;
+});
