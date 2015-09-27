9df50acc3c2ed035f71588177918a730c687d58d
vincentorback <vorback@gmail.com>
2015-09-28 00:24:26 +0200

    Added some WYSIWYG settings

--- wysiwyg.php
+++ wysiwyg.php
@@ -21,14 +21,18 @@ add_filter( 'mce_buttons_3', function ( $buttons ) {
 /**
  * Change default TinyMCE WYSIWYG settings.
  * https://codex.wordpress.org/TinyMCE
+ *
+ * @param $settings Object Array of TinyMCE settings
  */
-add_filter( 'tiny_mce_before_init', function ( $in ) {
-  $in['remove_linebreaks'] = true;
-  $in['gecko_spellcheck'] = true;
-  $in['accessibility_focus'] = true;
-  $in['paste_remove_styles'] = true;
-  $in['paste_remove_spans'] = true;
-  $in['paste_strip_class_attributes'] = 'all';
+add_filter( 'tiny_mce_before_init', function ( $settings ) {
+  $settings['remove_linebreaks'] = true;
+  $settings['gecko_spellcheck'] = true;
+  $settings['keep_styles'] = false;
+  $settings['accessibility_focus'] = true;
+  $settings['content_css'] = get_template_directory_uri() . '/custom-editor-style.css';
+  $settings['paste_remove_styles'] = true;
+  $settings['paste_remove_spans'] = true;
+  $settings['paste_strip_class_attributes'] = 'all';
 
-  return $in;
+  return $settings;
 });
