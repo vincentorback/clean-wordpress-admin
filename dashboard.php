d5cefc2db715aa30cacf9a5c427326555c9e4d10
vincentorback <vorback@gmail.com>
2015-09-28 00:24:33 +0200

    Cleaned up comments

--- dashboard.php
+++ dashboard.php
@@ -2,16 +2,32 @@
 
 /**
  * Removing unnecessary dashboard widgets.
- * This will also remove the capability to show them using the "Screen options"
  */
 add_action( 'admin_init', function () {
-  remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' ); //Removes the 'incoming links' widget
-  remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' ); //Removes the 'plugins' widget
-  remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' ); //Removes the 'WordPress News' widget
-  remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' ); //Removes the secondary widget
-  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' ); //Removes the 'Quick Draft' widget
-  remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' ); //Removes the 'Recent Drafts' widget
-  remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' ); //Removes the 'Activity' widget
-  remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' ); //Removes the 'At a Glance' widget
-  remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' ); //Removes the 'Activity' widget (since 3.8)
+  // Removes the 'incoming links' widget
+  remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+
+  // Removes the 'plugins' widget
+  remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+
+  // Removes the 'WordPress News' widget
+  remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
+
+  // Removes the secondary widget
+  remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+
+  // Removes the 'Quick Draft' widget
+  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+
+  // Removes the 'Recent Drafts' widget
+  remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+
+  // Removes the 'Activity' widget
+  remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+
+  // Removes the 'At a Glance' widget
+  remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+
+  // Removes the 'Activity' widget (since 3.8)
+  remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
 });
