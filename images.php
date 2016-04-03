a9e069aa3aa7eb57a9820a570edb355f2a437a55
vincentorback <vorback@gmail.com>
2016-04-03 16:11:35 +0200

    Added more function to set default options to image attachments

--- images.php
+++ images.php
@@ -1,10 +1,23 @@
 <?php
 
 /**
- * Remove default links on images
+ * Set default image attachment options
  */
-add_action( 'admin_init', function () {
+add_action( 'after_setup_theme', function () {
+
+  // Remove default link
   if ( get_option( 'image_default_link_type' ) !== 'none' ) {
     update_option( 'image_default_link_type', 'none' );
   }
+
+  // Remove default alignment
+  if ( get_option( 'image_default_align' ) !== 'none' ) {
+    update_option('image_default_align', 'none' );
+  }
+
+  // Set default size
+  if ( get_option( 'image_default_size' ) !== 'none' ) {
+    update_option('image_default_size', 'large' );
+  }
+
 });
