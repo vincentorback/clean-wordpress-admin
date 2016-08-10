9568e20e6f8584852a73dac12ee2dd163c8fe140
vincentorback <vorback@gmail.com>
2016-08-10 11:30:34 +0200

    Some extra functions

--- extras.php
+++ extras.php
@@ -4,7 +4,57 @@
  * Check if on posts-page
  */
 function is_page_for_posts () {
-  return ( is_home() || (is_archive() && ! is_post_type_archive() ) );
+  return ( is_home() || ( is_archive() && ! is_post_type_archive() ) );
+}
+
+
+/**
+ * Check if is search page
+ * @return boolean [description]
+ */
+function is_search_page () {
+  if ( is_search() ) {
+    return true;
+  }
+
+  $search_template_page = get_pages_with_template( 'search.php' );
+
+  if ( $search_template_page && is_page( $search_template_page[0]->ID ) ) {
+    return true;
+  }
+}
+
+
+/**
+ * Get pages using a specific template
+ * @param $template - Name of template file
+ */
+function get_pages_with_template( $template ) {
+  return get_pages(array(
+    'meta_key' => '_wp_page_template',
+    'meta_value' => $template
+  ));
+}
+
+
+/**
+ * Get page depth
+ */
+function get_page_depth ( $page ) {
+  if ( ! $page ) {
+    return;
+  }
+
+  $parent_id  = $page->post_parent;
+  $depth = 0;
+
+  while ( $parent_id > 0 ) {
+    $parent = get_page( $parent_id );
+    $parent_id = $parent->post_parent;
+    $depth++;
+  }
+
+  return $depth;
 }
 
 
@@ -67,3 +117,12 @@ add_filter( 'admin_footer_text', function () {
 add_filter( 'update_footer', function () {
  return '¯\_(ツ)_/¯';
 }, 999);
+
+
+/**
+ * Stay logged in for longer
+ * @link https://developer.wordpress.org/reference/hooks/auth_cookie_expiration/
+ */
+add_filter( 'auth_cookie_expiration', function () {
+  return 31556926; // 1 year
+});
