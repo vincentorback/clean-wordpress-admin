eee9bbc407cb2ac7a33a169ca569c177eb559904
vincentorback <vorback@gmail.com>
2016-08-30 21:32:49 +0200

    more emoji removal

--- emojis.php
+++ emojis.php
@@ -5,14 +5,16 @@
  * @link https://codex.wordpress.org/Emoji
  */
 add_action( 'init', function () {
-  // All actions related to emojis
-  remove_action( 'admin_print_styles', 'print_emoji_styles' );
   remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
   remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
   remove_action( 'wp_print_styles', 'print_emoji_styles' );
-  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
   remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
   remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+  add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+  add_filter( 'emoji_svg_url', '__return_false' );
 
   // Filter to remove TinyMCE emojis
   add_filter( 'tiny_mce_plugins', function ( $plugins ) {
