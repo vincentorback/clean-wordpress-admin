61422f55ab9ccbb20c26c102a3fcabcb93254e2d
vincentorback <vorback@gmail.com>
2017-05-16 21:56:59 +0200

    de-register wp-embed

--- javascript.php
+++ javascript.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * De-registers the WordPress stock jQuery script
+ * De-registers WordPress default javascript
  * @link https://codex.wordpress.org/Function_Reference/wp_deregister_script
  */
 add_action( 'wp_enqueue_scripts', function () {
   if ( !is_admin() ) {
      wp_deregister_script( 'jquery' );
+     wp_deregister_script( 'wp-embed' );
   }
 });
