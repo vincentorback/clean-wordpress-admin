0f98e9bcb7aabed16d131c7ffa2a278522c791d0
vincentorback <vorback@gmail.com>
2017-05-27 15:57:45 +0200

    remove more help-tabs

--- contextual-tabs.php
+++ contextual-tabs.php
@@ -14,8 +14,35 @@ add_filter( 'contextual_help', function ( $old_help, $screen_id, $screen ) {
 
   // Remove specific tabs
   $screen->remove_help_tab( 'overview' );
+
+  $screen->remove_help_tab( 'help-navigation' );
+  $screen->remove_help_tab( 'help-layout' );
+  $screen->remove_help_tab( 'help-content' );
+
+  $screen->remove_help_tab( 'attachment-details' );
   $screen->remove_help_tab( 'managing-pages' );
   $screen->remove_help_tab( 'managing-pages' );
+
+  $screen->remove_help_tab( 'moderating-comments' );
+
+  $screen->remove_help_tab( 'adding-themes' );
+  $screen->remove_help_tab( 'customize-preview-themes' );
+
+  $screen->remove_help_tab( 'compatibility-problems' );
+  $screen->remove_help_tab( 'adding-plugins' );
+
+  $screen->remove_help_tab( 'screen-display' );
+  $screen->remove_help_tab( 'actions' );
+  $screen->remove_help_tab( 'user-roles' );
+
+  $screen->remove_help_tab( 'press-this' );
+  $screen->remove_help_tab( 'converter' );
+
+  $screen->remove_help_tab( 'options-postemail' );
+  $screen->remove_help_tab( 'options-services' );
+  $screen->remove_help_tab( 'site-visibility' );
+  $screen->remove_help_tab( 'permalink-settings' );
+  $screen->remove_help_tab( 'custom-structures' );
 }, 999, 3);
 
 
