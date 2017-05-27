50dab9023ef42d59694b65fe5e6098a3453fab91
vincentorback <vorback@gmail.com>
2017-05-27 14:46:37 +0200

    remove check for is_super_admin

--- notifications.php
+++ notifications.php
@@ -4,12 +4,6 @@
  * Hide core updates
  */
 add_action( 'after_setup_theme', function () {
-
-  // Still show updates to superadmins
-  if ( (! current_user_can( 'update_core' )) || is_super_admin() ) {
-    return;
-  }
-
   add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
   add_filter( 'pre_option_update_core', '__return_null' );
   add_filter( 'pre_site_transient_update_core', '__return_null' );
