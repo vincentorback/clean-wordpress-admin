ffd7029efa008bf7040f43e8a2b72bfe482c4048
vincentorback <vorback@gmail.com>
2017-05-27 15:57:54 +0200

    clean up image functions

--- images.php
+++ images.php
@@ -16,18 +16,10 @@ add_action( 'after_setup_theme', function () {
   }
 
   // Set default size
-  if ( get_option( 'image_default_size' ) !== 'none' ) {
+  if ( get_option( 'image_default_size' ) !== 'large' ) {
     update_option( 'image_default_size', 'large' );
   }
 
-});
-
-
-/**
- * Remove WordPress Gallery
- */
-add_action( 'admin_enqueue_scripts', function () {
-  wp_deregister_script( 'admin-gallery' );
 });
 
 
@@ -38,15 +30,18 @@ add_filter( 'wp_calculate_image_srcset', '__return_false' );
 
 
 /**
- * Remove image size attributes
+ * Remove size attributes
  * @param  String $html
  */
-function remove_image_sizes( $html ) {
+function remove_sizes( $html ) {
   return preg_replace( '/(width|height)="\d*"/', '', $html );
 }
 
 // Remove size attributes from thumbnail images
-add_filter( 'post_thumbnail_html', 'remove_image_sizes' );
+add_filter( 'post_thumbnail_html', 'remove_sizes' );
 
-// Remove size attributes from images in posts
-add_filter( 'image_send_to_editor', 'remove_image_sizes' );
+// Remove size attributes in the editor
+add_filter( 'image_send_to_editor', 'remove_sizes' );
+
+// Remove size attributes from the_content
+add_filter( 'the_content', 'remove_sizes' );
