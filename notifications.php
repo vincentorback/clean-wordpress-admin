7c4c2ff89db014bf2de272c463271494400ae5a0
vincentorback <vorback@gmail.com>
2017-05-27 15:56:56 +0200

    lint "Disable inactive plugins"-function

--- notifications.php
+++ notifications.php
@@ -22,19 +22,20 @@ add_filter( 'pre_site_transient_update_plugins', '__return_null' );
  * Disable inactive plugins update notifications
  */
 add_filter( 'transient_update_plugins', function ( $value = '' ) {
-  if ((isset($value->response)) && (count($value->response))) {
+  if ( ( isset( $value->response ) ) && ( count( $value->response ) ) ) {
     $active_plugins = get_option( 'active_plugins' );
-    if ($active_plugins) {
-      foreach($value->response as $plugin_idx => $plugin_item) {
-        if (!in_array($plugin_idx, $active_plugins)) {
-          unset($value->response[$plugin_idx]);
+    if ( $active_plugins ) {
+      foreach( $value->response as $plugin_idx => $plugin_item ) {
+        if ( ! in_array( $plugin_idx, $active_plugins ) ) {
+          unset( $value->response[$plugin_idx] );
         }
       }
     } else {
-      foreach($value->response as $plugin_idx => $plugin_item) {
-        unset($value->response);
+      foreach( $value->response as $plugin_idx => $plugin_item ) {
+        unset( $value->response );
       }
     }
   }
+
   return $value;
 });
