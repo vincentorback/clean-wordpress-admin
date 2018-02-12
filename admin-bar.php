f103a81dddc4b8a1d3ebc50cd7d6e3209e0cda91
Vincent Orback <vorback@gmail.com>
2018-02-12 14:04:16 +0100

    remove more items in the admin_bar

--- admin-bar.php
+++ admin-bar.php
@@ -13,21 +13,25 @@ add_filter( 'show_admin_bar', '__return_false' );
  *
  * Indentation shows sub-items.
  */
-add_action( 'admin_bar_menu', function ( $admin_bar ) {
+add_action( 'wp_before_admin_bar_render', function () {
+  global $wp_admin_bar;
 
-  $admin_bar->remove_menu( 'wp-logo' );            // Remove the WordPress logo
-    $admin_bar->remove_menu( 'about' );            // Remove the about WordPress link
-    $admin_bar->remove_menu( 'wporg' );            // Remove the about WordPress link
-    $admin_bar->remove_menu( 'documentation' );    // Remove the WordPress documentation link
-    $admin_bar->remove_menu( 'support-forums' );   // Remove the support forums link
-    $admin_bar->remove_menu( 'feedback' );         // Remove the feedback link
+  $wp_admin_bar->remove_menu( 'wp-logo' );            // Remove the WordPress logo
+    $wp_admin_bar->remove_menu( 'about' );            // Remove the about WordPress link
+    $wp_admin_bar->remove_menu( 'wporg' );            // Remove the about WordPress link
+    $wp_admin_bar->remove_menu( 'documentation' );    // Remove the WordPress documentation link
+    $wp_admin_bar->remove_menu( 'support-forums' );   // Remove the support forums link
+    $wp_admin_bar->remove_menu( 'feedback' );         // Remove the feedback link
 
-  $admin_bar->remove_menu( 'site-name' );          // Remove the site name menu
-    $admin_bar->remove_menu( 'view-site' );        // Remove the view site link
+  $wp_admin_bar->remove_menu( 'site-name' );          // Remove the site name menu
+    $wp_admin_bar->remove_menu( 'view-site' );        // Remove the view site link
+    $wp_admin_bar->remove_menu( 'dashboard' );           // Remove the dashboard link
+    $wp_admin_bar->remove_menu( 'menus' );               // Remove the menus link
 
-  $admin_bar->remove_menu( 'updates' );            // Remove the updates link
-  $admin_bar->remove_menu( 'comments' );           // Remove the comments link
-  $admin_bar->remove_menu( 'new-content' );        // Remove the content link
-  $admin_bar->remove_menu( 'my-account' );         // Remove the user details tab
+  $wp_admin_bar->remove_menu( 'updates' );            // Remove the updates link
+  $wp_admin_bar->remove_menu( 'comments' );           // Remove the comments link
+  $wp_admin_bar->remove_menu( 'new-content' );        // Remove the content link
+  $wp_admin_bar->remove_menu( 'my-account' );         // Remove the user details tab
+  $wp_admin_bar->remove_menu( 'search' );         // Remove the search tab
 
 }, 999); // Needs to have low priority
