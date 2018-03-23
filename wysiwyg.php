6aa240c8cbe60934bb2811d7096f762c3b262701
Vincent Orback <vorback@gmail.com>
2018-03-23 16:52:53 +0100

    remove wysiwyg that adds rather than clean

--- wysiwyg.php
+++ wysiwyg.php
@@ -22,36 +22,16 @@ add_filter( 'user_can_richedit' , '__return_false', 50 );
  */
 add_filter( 'tiny_mce_before_init', function ( $settings ) {
   $settings['remove_linebreaks'] = true;
-  $settings['gecko_spellcheck'] = true;
   $settings['keep_styles'] = false;
   $settings['accessibility_focus'] = true;
-  $settings['content_css'] = 'path/to/custom-editor-style.css';
   $settings['paste_remove_styles'] = true;
   $settings['paste_remove_spans'] = true;
   $settings['paste_strip_class_attributes'] = 'all';
-
-  $settings['toolbar1'] = 'formatselect,bold,italic,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,underline';
-  $settings['toolbar2'] = '';
-
-  $settings['block_formats'] = "Paragraph=p; Heading 1=h1; Heading 2=h2; Heading 3=h3;";
-
-  // Create your own JavaScript preprocessor that triggers before pasting
-  $settings['paste_preprocess'] = "function (plugin, args) {
-    // Strip all HTML tags except those we have whitelisted
-    var whitelist = 'p,span,b,strong,i,em,h3,h4,h5,h6,ul,li,ol';
-    var stripped = $('<div>' + args.content + '</div>');
-    var els = stripped.find('*').not(whitelist);
-
-    for (var i = els.length - 1; i >= 0; i--) {
-      var e = els[i];
-      $(e).replaceWith(e.innerHTML);
-    }
-    // Strip all class and id attributes
-    stripped.find('*').removeAttr('id').removeAttr('class');
-
-    // Return the clean HTML
-    args.content = stripped.html();
-  }";
+  $settings['block_formats'] = "Paragraph=p; Heading 1=h1; Heading 2=h2";
+  $settings['toolbar1'] = 'bold,italic,link,unlink';
+  $settings['toolbar2'] = 'formatselect,removeformat';
+  $settings['toolbar3'] = '';
+  $settings['toolbar4'] = '';
 
   return $settings;
 });
