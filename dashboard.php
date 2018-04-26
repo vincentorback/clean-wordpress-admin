7e9fb6085c7a114be4ea4b9b63850d179f3302fd
Vincent Orback <vorback@gmail.com>
2018-04-26 13:41:15 +0200

    remove removal of old dashboard metaboxes

--- dashboard.php
+++ dashboard.php
@@ -8,38 +8,22 @@ add_action( 'admin_init', function () {
   // Remove the 'Welcome' panel
   remove_action('welcome_panel', 'wp_welcome_panel');
 
-  // Remove the 'incoming links' metabox
-  remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
-
-  // Remove the 'plugins' metabox
-  remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
-
-  // Remove the 'WordPress News' metabox
-  remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
-
-  // Remove the secondary metabox
-  remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
-
-  // Remove the 'Quick Draft' metabox
-  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
-
-  // Remove the 'Recent Drafts' metabox
-  remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
-
-  // Remove the 'Activity' metabox
-  remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-
   // Remove the 'At a Glance' metabox
   remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
 
-  // Remove the 'Activity' metabox (since 3.8)
+  // Remove the 'Activity' metabox
   remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
-});
 
+  // Remove the 'WordPress News' metabox
+  remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+
+  // Remove the 'Quick Draft' metabox
+  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+});
 
 /**
  * Remove access to the dashboard
- */
+*/
 add_action( 'admin_init', function () {
   global $pagenow; // Get current page
   $redirect = get_admin_url( null, 'edit.php' ); // Where to redirect
