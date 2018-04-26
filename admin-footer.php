68794500207af5e368955be0636bd6c801e447e5
Vincent Orback <vorback@gmail.com>
2018-04-26 13:41:26 +0200

    use __return_empty_string

--- admin-footer.php
+++ admin-footer.php
@@ -4,15 +4,11 @@
  * Remove left admin footer text
  * @link https://developer.wordpress.org/reference/hooks/admin_footer_text/
  */
-add_filter( 'admin_footer_text', function () {
-  return '';
-}, 999);
+add_filter( 'admin_footer_text', '__return_empty_string' );
 
 
 /**
  * Remove right admin footer text (where the WordPress version nr is)
  * @link https://developer.wordpress.org/reference/hooks/update_footer/
  */
-add_filter( 'update_footer', function () {
- return '';
-}, 999);
+add_filter( 'update_footer', '__return_empty_string', 11 );
