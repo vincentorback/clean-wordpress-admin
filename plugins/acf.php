7c2ffbb53dc3242a5c232bfbef46fe4918b39e92
Vincent Orback <vorback@gmail.com>
2018-06-15 12:03:41 +0200

    hide acf pages from admin menu

--- plugins/acf.php
+++ plugins/acf.php
@@ -5,4 +5,19 @@
  */
 add_filter('acf/settings/show_admin', '__return_false');
 
+/**
+ * Remove ACF from admin menu
+ */
+add_action( 'admin_menu', function () {
+  // Remove ACF
+  remove_menu_page('edit.php?post_type=acf-field-group');
 
+  // Remove ACF -> Edit
+  remove_submenu_page('edit.php?post_type=acf-field-group', 'post-new.php?post_type=acf-field-group');
+
+  // Remove ACF -> Tools
+  remove_submenu_page('edit.php?post_type=acf-field-group', 'acf-settings-tools');
+
+  // Remove ACF -> Updates
+  remove_submenu_page('edit.php?post_type=acf-field-group', 'acf-settings-updates');
+}, 999);
