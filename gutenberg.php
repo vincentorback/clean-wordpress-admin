1aff84aa3b477a10edcc7b439b3c1be99c4cce13
Vincent Orback <hello@vincentorback.se>
2019-09-12 12:17:00 +0200

    Update gutenberg filters + disable gutenberg for IDs and templates

--- gutenberg.php
+++ gutenberg.php
@@ -8,11 +8,24 @@ add_filter('gutenberg_can_edit_post_type', '__return_false');
 
 
 /**
- * Disable Gutenberg for a specific post type
+ * Disable Gutenberg for a specific posts, post types, templates, etc.
  */
-add_filter('gutenberg_can_edit_post_type', function ($is_enabled, $post_type) {
-  if ($post_type === 'post') return false;
-  
-  return $is_enabled;
-});
+add_filter('use_block_editor_for_post', function ($use_block_editor, $post) {
+  // Disable for specific post ID
+  if ($post->ID === 123) {
+    return false;
+  }
 
+  // Disable for post type
+  if ($post->post_type == 'event') {
+    return false;
+  }
+
+  // Disable for page template
+  if (basename(get_page_template($post->ID)) == 'template-events.php') {
+    return false;
+  }
+
+  // Return default value
+  return $use_block_editor;
+}, 10, 2);
