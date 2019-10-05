220be4b78bb5c6ec80154990a2746451ac997b5c
Vincent Orback <hello@vincentorback.se>
2019-10-05 13:20:20 +0200

    more ways to disable emojis

--- emojis.php
+++ emojis.php
@@ -4,27 +4,40 @@
  * Remove emoji support
  * @link https://codex.wordpress.org/Emoji
  */
-add_action( 'init', function () {
-  // Front-end
-  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-  remove_action( 'wp_print_styles', 'print_emoji_styles' );
-  add_filter( 'emoji_svg_url', '__return_false' );
 
-  // Admin
-  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-  remove_action( 'admin_print_styles', 'print_emoji_styles' );
-  add_filter( 'tiny_mce_plugins', function ( $plugins ) {
+add_action('init', function () {
+  // Front-end
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+	// Admin
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+	// Feeds
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji');
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji');
+
+	// Embeds
+	remove_filter( 'embed_head', 'print_emoji_detection_script' );
+
+	// Emails
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
+	// Disable from TinyMCE editor. Disabled in block editor by default
+	add_filter( 'tiny_mce_plugins', function ( $plugins ) {
     if ( is_array( $plugins ) ) {
-      return array_diff( $plugins, array( 'wpemoji' ) );
+      $plugins = array_diff( $plugins, array( 'wpemoji' ) );
     }
 
-    return array();
+    return $plugins;
   });
 
-  // Feeds
-  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-
-  // Emails
-  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	/** Finally, disable it from the database also, to prevent characters from converting
+	 *  There used to be a setting under Writings to do this
+	 *  Not ideal to get & update it here - but it works :/
+	 */
+	if ( (int) get_option( 'use_smilies' ) === 1 ) {
+		update_option( 'use_smilies', 0 );
+	}
 });
