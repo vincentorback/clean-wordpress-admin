495d9c0131894751719fe68f1c5a2ce7114d17bd
Vincent Orback <hello@vincentorback.se>
2019-12-15 19:58:43 +0100

    lint + remove yoast seo roles

--- plugins/yoast-seo.php
+++ plugins/yoast-seo.php
@@ -3,61 +3,80 @@
 /**
  * Move Yoast Meta Box to bottom
  */
-add_filter( 'wpseo_metabox_prio', function() {
-  return 'low';
-}, 10);
+add_filter(
+    'wpseo_metabox_prio', function () {
+        return 'low';
+    }, 10
+);
 
 /**
  * Removes Yoast SEO comment in head
  */
-if ( isset( $GLOBALS['wpseo_front'] ) && is_a( $GLOBALS['wpseo_front'], 'WPSEO_Frontend' ) ) {
-  remove_action( 'wpseo_head', array( $GLOBALS['wpseo_front'], 'debug_marker' ), 2 );
+if (isset($GLOBALS['wpseo_front']) && is_a($GLOBALS['wpseo_front'], 'WPSEO_Frontend') ) {
+    remove_action('wpseo_head', array( $GLOBALS['wpseo_front'], 'debug_marker' ), 2);
 }
 
 /**
  * Remove Yoast from admin bar
  */
-add_action( 'wp_before_admin_bar_render', function () {
-  global $wp_admin_bar;
+add_action(
+    'wp_before_admin_bar_render', function () {
+        global $wp_admin_bar;
 
-  $wp_admin_bar->remove_menu( 'wpseo-menu' );
-}, 999);
+        $wp_admin_bar->remove_menu('wpseo-menu');
+    }, 999
+);
 
 /**
  * Remove 'primary' categories
  */
-add_filter( 'wpseo_primary_term_taxonomies', '__return_false' );
+add_filter('wpseo_primary_term_taxonomies', '__return_false');
 
 /**
  * Remove post columns
  */
-add_filter( 'manage_posts_columns', function ( $columns ) {
-  unset(
-    $columns['categories'],
-    $columns['tags'],
-    $columns['comments'],
+add_filter(
+    'manage_posts_columns', function ( $columns ) {
+        unset(
+            $columns['wpseo-score'],
+            $columns['wpseo-title'],
+            $columns['wpseo-metadesc'],
+            $columns['wpseo-focuskw'],
+            $coulmns['wpseo-links']
+        );
 
-    $columns['wpseo-score'],
-    $columns['wpseo-title'],
-    $columns['wpseo-metadesc'],
-    $columns['wpseo-focuskw'],
-    $coulmns['wpseo-links']
-  );
-
-  return $columns;
-});
+        return $columns;
+    }
+);
 
 /**
  * Remove page columns
  */
-add_filter( 'manage_page_columns', function ( $columns ) {
-  unset(
-    $columns['wpseo-score'],
-    $columns['wpseo-title'],
-    $columns['wpseo-metadesc'],
-    $columns['wpseo-focuskw'],
-    $columns['wpseo-links']
-  );
+add_filter(
+    'manage_page_columns', function ( $columns ) {
+        unset(
+            $columns['wpseo-score'],
+            $columns['wpseo-title'],
+            $columns['wpseo-metadesc'],
+            $columns['wpseo-focuskw'],
+            $columns['wpseo-links']
+        );
 
-  return $columns;
-});
+        return $columns;
+    }
+);
+
+/**
+ * Remove Yoast roles
+ */
+add_action(
+    'admin_init', function () {
+        if (get_role('wpseo_manager')) {
+            remove_role('wpseo_manager');
+        }
+
+        if (get_role('wpseo_editor')) {
+            remove_role('wpseo_editor');
+        }
+    }
+);
