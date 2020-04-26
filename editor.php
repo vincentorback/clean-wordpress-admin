daa40bcbbcc8796b7a14af44d20196b0b439e6dd
Vincent Orback <hello@vincentorback.se>
2020-04-26 12:40:57 +0200

    fix indent

--- editor.php
+++ editor.php
@@ -6,7 +6,7 @@
  * @link https://developer.wordpress.org/reference/hooks/use_block_editor_for_post/
  */
 
-add_filter('use_block_editor_for_post', '__return_false');
+add_filter('use_block_editor_for_post', '__return_false', 10, 2);
 
 
 /**
@@ -124,90 +124,89 @@ add_action('after_setup_theme', function () {
 add_filter(
     'allowed_block_types', function ($allowed_blocks, $post) {
         $allowed_blocks = array(
+            // Common
+            'core/paragraph',
+            'core/image',
+            'core/heading',
+            'core/gallery',
+            'core/list',
+            'core/quote',
+            'core/audio',
+            'core/cover', // previously core/cover-image
+            'core/file',
+            'core/video',
 
-        // Common
-        'core/paragraph',
-        'core/image',
-        'core/heading',
-        'core/gallery',
-        'core/list',
-        'core/quote',
-        'core/audio',
-        'core/cover', // previously core/cover-image
-        'core/file',
-        'core/video',
+            // Formatting
+            'core/table',
+            'core/verse',
+            'core/code',
+            'core/freeform',
+            'core/html',
+            'core/preformatted',
+            'core/pullquote',
 
-        // Formatting
-        'core/table',
-        'core/verse',
-        'core/code',
-        'core/freeform',
-        'core/html',
-        'core/preformatted',
-        'core/pullquote',
+            // Layout Elements
+            'core/button',
+            'core/text-columns',
+            'core/media-text',
+            'core/more',
+            'core/nextpage',
+            'core/separator',
+            'core/spacer',
 
-        // Layout Elements
-        'core/button',
-        'core/text-columns',
-        'core/media-text',
-        'core/more',
-        'core/nextpage',
-        'core/separator',
-        'core/spacer',
+            // Widgets
+            'core/shortcode',
+            'core/archives',
+            'core/categories',
+            'core/latest-comments',
+            'core/latest-posts',
+            'core/calendar',
+            'core/rss',
+            'core/search',
+            'core/tag-cloud',
 
-        // Widgets
-        'core/shortcode',
-        'core/archives',
-        'core/categories',
-        'core/latest-comments',
-        'core/latest-posts',
-        'core/calendar',
-        'core/rss',
-        'core/search',
-        'core/tag-cloud',
-
-        // Embeds
-        'core/embed',
-        'core-embed/twitter',
-        'core-embed/youtube',
-        'core-embed/facebook',
-        'core-embed/instagram',
-        'core-embed/wordpress',
-        'core-embed/soundcloud',
-        'core-embed/spotify',
-        'core-embed/flickr',
-        'core-embed/vimeo',
-        'core-embed/animoto',
-        'core-embed/cloudup',
-        'core-embed/collegehumor',
-        'core-embed/dailymotion',
-        'core-embed/funnyordie',
-        'core-embed/hulu',
-        'core-embed/imgur',
-        'core-embed/issuu',
-        'core-embed/kickstarter',
-        'core-embed/meetup-com',
-        'core-embed/mixcloud',
-        'core-embed/photobucket',
-        'core-embed/polldaddy',
-        'core-embed/reddit',
-        'core-embed/reverbnation',
-        'core-embed/screencast',
-        'core-embed/scribd',
-        'core-embed/slideshare',
-        'core-embed/smugmug',
-        'core-embed/speaker',
-        'core-embed/ted',
-        'core-embed/tumblr',
-        'core-embed/videopress',
-        'core-embed/wordpress-tv'
+            // Embeds
+            'core/embed',
+            'core-embed/twitter',
+            'core-embed/youtube',
+            'core-embed/facebook',
+            'core-embed/instagram',
+            'core-embed/wordpress',
+            'core-embed/soundcloud',
+            'core-embed/spotify',
+            'core-embed/flickr',
+            'core-embed/vimeo',
+            'core-embed/animoto',
+            'core-embed/cloudup',
+            'core-embed/collegehumor',
+            'core-embed/dailymotion',
+            'core-embed/funnyordie',
+            'core-embed/hulu',
+            'core-embed/imgur',
+            'core-embed/issuu',
+            'core-embed/kickstarter',
+            'core-embed/meetup-com',
+            'core-embed/mixcloud',
+            'core-embed/photobucket',
+            'core-embed/polldaddy',
+            'core-embed/reddit',
+            'core-embed/reverbnation',
+            'core-embed/screencast',
+            'core-embed/scribd',
+            'core-embed/slideshare',
+            'core-embed/smugmug',
+            'core-embed/speaker',
+            'core-embed/ted',
+            'core-embed/tumblr',
+            'core-embed/videopress',
+            'core-embed/wordpress-tv',
         );
 
         // Can also be specified by posts
         if ($post->post_type === 'post') {
             $allowed_blocks = array(
-            'core/paragraph',
-            'core/heading'
+                'core/paragraph',
+                'core/heading',
             );
         }
 
