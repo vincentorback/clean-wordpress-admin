c1658b2eebf990b8e1c1cec4e90f38618cea69c9
Vincent Orback <hello@vincentorback.se>
2020-12-19 13:30:31 +0100

    move classic editor functions

--- editor.php
+++ editor.php
@@ -39,68 +39,6 @@ add_filter(
 	2
 );
 
-
-/**
-* Remove post type metaboxes
- *
-* @link https://developer.wordpress.org/reference/functions/remove_meta_box/
-*/
-add_action(
-	'admin_menu',
-	function () {
-		$post_type = 'post';
-		$context   = 'normal';
-
-		// Author metabox
-		remove_meta_box( 'authordiv', $post_type, $context );
-
-		// Categories metabox.
-		remove_meta_box( 'categorydiv', $post_type, $context );
-
-		// Comments status metabox (discussion)
-		remove_meta_box( 'commentstatusdiv', $post_type, $context );
-
-		// Comments metabox
-		remove_meta_box( 'commentsdiv', $post_type, $context );
-
-		// Formats metabox
-		remove_meta_box( 'formatdiv', $post_type, $context );
-
-		// Attributes metabox
-		remove_meta_box( 'pageparentdiv', $post_type, $context );
-
-		// Custom fields metabox
-		remove_meta_box( 'postcustom', $post_type, $context );
-
-		// Excerpt metabox
-		remove_meta_box( 'postexcerpt', $post_type, $context );
-
-		// Featured image metabox
-		remove_meta_box( 'postimagediv', $post_type, $context );
-
-		// Revisions metabox
-		remove_meta_box( 'revisionsdiv', $post_type, $context );
-
-		// Slug metabox
-		remove_meta_box( 'slugdiv', $post_type, $context );
-
-		// Date, status, and update/save metabox
-		remove_meta_box( 'submitdiv', $post_type, $context );
-
-		// Tags metabox
-		remove_meta_box( 'tagsdiv-post_tag', $post_type, $context );
-
-		// Custom taxonomies metabox
-		remove_meta_box( 'tagsdiv-{$tax-name}', $post_type, $context );
-
-		// Hierarchical custom taxonomies metabox
-		remove_meta_box( '{$tax-name}div', $post_type, $context );
-
-		// Trackbacks metabox
-		remove_meta_box( 'trackbacksdiv', $post_type, $context );
-	}
-);
-
 add_action(
 	'after_setup_theme',
 	function () {
