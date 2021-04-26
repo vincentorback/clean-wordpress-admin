0669dde5440a09ecf6809a5e3350afa66e1deb9e
Vincent Orback <hello@vincentorback.se>
2021-04-26 19:41:59 +0200

    remove more fields from user pages

--- users.php
+++ users.php
@@ -1,6 +1,29 @@
 <?php
 
 /**
+ * Remove social contact methods
+ */
+add_filter(
+	'user_contactmethods',
+	function ( $contact_methods ) {
+		unset( $contact_methods['facebook'] );
+		unset( $contact_methods['instagram'] );
+		unset( $contact_methods['linkedin'] );
+		unset( $contact_methods['myspace'] );
+		unset( $contact_methods['pinterest'] );
+		unset( $contact_methods['soundcloud'] );
+		unset( $contact_methods['tumblr'] );
+		unset( $contact_methods['twitter'] );
+		unset( $contact_methods['youtube'] );
+		unset( $contact_methods['wikipedia'] );
+
+		return $contact_methods; // or you could just return an empty array to remove them all
+	}
+);
+
+
+
+/**
  * Hide profile fields
  *
  * @link https://developer.wordpress.org/reference/hooks/admin_print_scripts-hook_suffix/
@@ -11,41 +34,36 @@
  * Keyboard Short - .user-comment-shortcuts-wrap
  * Show Toolbar - .show-admin-bar
  * Language - .user-language-wrap
+ *
+ * First name - .user-first-name-wrap
+ * Last name - .user-last-name-wrap
+ * Nickname - .user-nickname-wrap
  * Biographical Info - .user-description-wrap
  */
 
-function hide_profile_fields_with_css() {
-	?><style>
-	.user-rich-editing-wrap,
-	.user-syntax-highlighting-wrap,
-	.user-comment-shortcuts-wrap,
-	.user-admin-color-wrap,
-	.user-url-wrap {
-	display: none;
-	}</style>
-	<?php
-}
+if ( ! function_exists( 'hide_profile_fields_with_css' ) ) {
+	function hide_profile_fields_with_css() {
+		?><style>
+		.user-rich-editing-wrap,
+		.user-syntax-highlighting-wrap,
+		.user-admin-color-wrap,
+		.user-comment-shortcuts-wrap,
+		.show-admin-bar,
+		.user-language-wrap,
+		.user-first-name-wrap,
+		.user-last-name-wrap,
+		.user-description-wrap,
+		.user-url-wrap {
+		display: none;
+		}</style>
+		<?php
+	}
 
-function hide_profile_fields_with_javascript() {
-	$fields = array(
-		'first_name',
-		'last_name',
-		'url',
-		'role',
+	add_action(
+		'admin_init',
+		function () {
+			add_action( 'admin_print_scripts-profile.php', 'hide_profile_fields_with_css' );
+			add_action( 'admin_print_scripts-user-edit.php', 'hide_profile_fields_with_css' );
+		}
 	);
-
-	echo '<script>jQuery(document).ready(function(){';
-	foreach ( $fields as $field ) {
-		echo "jQuery('#{$field}').parents('tr').remove();";
-	}
-	echo '})</script>';
 }
-
-add_action(
-	'admin_init',
-	function () {
-		add_action( 'admin_print_scripts-profile.php', 'hide_profile_fields_with_css' );
-		add_action( 'admin_print_scripts-user-edit.php', 'hide_profile_fields_with_css' );
-		add_action( 'admin_print_scripts-user-new.php', 'hide_profile_fields_with_javascript' );
-	}
-);
