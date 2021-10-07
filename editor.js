841bd857df6adf3addd86bb2e7fdc2f206590a12
Vincent Orback <hello@vincentorback.se>
2021-10-07 10:53:18 +0200

    remove color settings on paragraphs and hide download button for links by default

--- editor.js
+++ editor.js
@@ -1,60 +1,79 @@
 wp.domReady( function () {
-	/**
-	 * Remove editor panels
-	 */
-	wp.data
-		.dispatch( 'core/edit-post' )
-		.removeEditorPanel( 'taxonomy-panel-post_tag' );
+  /**
+   * Remove editor panels
+   */
+  wp.data
+    .dispatch( 'core/edit-post' )
+    .removeEditorPanel( 'taxonomy-panel-post_tag' )
 
   wp.data
     .dispatch( 'core/edit-post' )
-    .removeEditorPanel( 'page-attributes' );
+    .removeEditorPanel( 'page-attributes' )
 
-	/**
-	 * Remove rich text formats from rich text blocks.
-	 */
-	wp.richText.unregisterFormatType( 'core/bold' );
-	wp.richText.unregisterFormatType( 'core/code' );
-	wp.richText.unregisterFormatType( 'core/image' );
-	wp.richText.unregisterFormatType( 'core/italic' );
-	wp.richText.unregisterFormatType( 'core/strikethrough' );
-	wp.richText.unregisterFormatType( 'core/underline' );
-} );
+  /**
+   * Remove rich text formats from rich text blocks.
+   */
+  wp.richText.unregisterFormatType( 'core/bold' )
+  wp.richText.unregisterFormatType( 'core/code' )
+  wp.richText.unregisterFormatType( 'core/image' )
+  wp.richText.unregisterFormatType( 'core/italic' )
+  wp.richText.unregisterFormatType( 'core/strikethrough' )
+  wp.richText.unregisterFormatType( 'core/underline' )
+})
 
 /**
  * Remove styles from blocks
  */
 wp.hooks.addFilter(
-	'blocks.registerBlockType',
-	'your_namespace/editor',
-	function ( settings, name ) {
-		switch ( name ) {
-			// Separator block
-			case 'core/separator':
-				if ( settings.styles ) {
-					settings.styles = settings.styles.filter( function (
-						style
-					) {
-						return [ 'wide', 'dots' ].indexOf( style.name ) === -1; // Removing styles 'wide' and 'dots'
-					} );
-				}
-			case 'core/image':
-				if ( settings.styles && Array.isArray( settings.styles ) ) {
-					settings.styles = settings.styles.filter( ( style ) => {
-						return [ 'rounded' ].indexOf( style.name ) === -1; // Removing styles 'rounded'
-					} );
-				}
-				break;
-		}
+  'blocks.registerBlockType',
+  'your_namespace/editor',
+  function ( settings, name ) {
+    switch ( name ) {
 
-		return settings;
-	}
-);
+      // Paragraph block
+      case 'core/paragraph':
+        settings.supports.color = false // Disable color settings
+        break
+
+      // List block
+      case 'core/list':
+        settings.supports.color = false // Disable color settings
+        break
+
+      // Separator block
+      case 'core/separator':
+        if ( settings.styles ) {
+          settings.styles = settings.styles.filter( function (
+            style
+          ) {
+            return [ 'wide', 'dots' ].indexOf( style.name ) === -1 // Removing styles 'wide' and 'dots'
+          } )
+        }
+        break
+
+      // Image bock
+      case 'core/image':
+        if ( settings.styles && Array.isArray( settings.styles ) ) {
+          settings.styles = settings.styles.filter( function ( style ) {
+            return [ 'rounded' ].indexOf( style.name ) === -1 // Removing styles 'rounded'
+          } )
+        }
+        break
+
+      // Image bock
+      case 'core/file':
+        settings.attributes.showDownloadButton.default = false // Hide download button from file links
+        break
+    }
+
+    return settings
+  }
+)
 
 /**
  * Unregister plugins
  */
-wp.domReady(() => {
+wp.domReady( function () {
   // You can log the IDs registered plugins to see which you can remove
   console.log(wp.plugins.getPlugins())
 
