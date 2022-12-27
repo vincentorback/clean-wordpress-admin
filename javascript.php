082bfe78fcccc0a9ec5aa06c34c7d1f22f2f3e87
Vincent Orback <hello@vincentorback.se>
2022-12-27 19:06:44 +0100

    remove wordpress version parameter from scripts

--- javascript.php
+++ javascript.php
@@ -18,3 +18,17 @@ add_action(
  * @link https://developer.wordpress.org/reference/functions/wp_oembed_add_host_js/
  */
 remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+
+/**
+ * Remove wordpress version from scripts
+ */
+function remove_script_version ($src)
+{
+  global $wp_version;
+
+  $parts = explode( "?ver=$wp_version", $src );
+  return $parts[0];
+}
+
+add_filter( 'script_loader_src', 'remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', 'remove_script_version', 15, 1 );
