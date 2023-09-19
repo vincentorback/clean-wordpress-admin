9fe66e0934b69e6ffd5aeea81824806cb800d96b
Vincent Orback <hello@vincentorback.se>
2023-09-19 12:58:45 +0200

    hide ”users” endpoint in rest api

--- rest-api.php
+++ rest-api.php
@@ -1,7 +1,52 @@
 <?php
 
 /**
- * Disable the REST API
+ * Prefix REST API url
+ * Default: "wp-json"
+ * @link https://developer.wordpress.org/reference/hooks/rest_url_prefix/
+ */
+add_filter( 'rest_url_prefix', function () {
+	return 'my_new_api_prefix';
+});
+
+
+
+/**
+ * Hide REST API endpoints from non-authenticated users
+ * In this example we hide the "users" endpoint
+ * @link https://wordpress.stackexchange.com/a/378142/27127
+ */
+function users_permission_check ($existing_callback) {
+  return function ($request) use($existing_callback) {
+      if (! current_user_can('list_users')) {
+          return new WP_Error(
+              'rest_user_cannot_view',
+              __( 'Sorry, you are not allowed to access users.' ),
+              [ 'status' => rest_authorization_required_code() ]
+          );
+      }
+
+      return $existing_callback($request);
+  };
+}
+
+function api_users_endpoint_force_auth ($endpoints) {
+  $users_get_route = &$endpoints['/wp/v2/users'][0];
+  $users_get_route['permission_callback'] = users_permission_check($users_get_route['permission_callback']);
+
+  $user_get_route = &$endpoints['/wp/v2/users/(?P<id>[\d]+)'][0];
+  $user_get_route['permission_callback'] = users_permission_check($user_get_route['permission_callback']);
+
+  return $endpoints;
+}
+
+add_filter( 'rest_endpoints', 'api_users_endpoint_force_auth' );
+
+
+
+/**
+ * Fullt disable the REST API
+ * (not recommended, will probably break stuff)
  */
 add_action(
 	'init',
@@ -13,14 +58,3 @@ add_action(
 		remove_action( 'template_redirect', 'rest_output_link_header', 11 );
 	}
 );
-
-
-/**
- * Prefix REST API url
- * Default: "wp-json"
- * @link https://developer.wordpress.org/reference/hooks/rest_url_prefix/
- */
-add_filter( 'rest_url_prefix', function () {
-	return 'my_new_api_prefix';
-});
-
