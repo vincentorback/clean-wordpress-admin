d8aa14a0aed3ead5b4e82b0bab16e59f21ede66b
Vincent Orback <hello@vincentorback.se>
2025-04-12 13:43:55 +0200

    disable more things in the gutenberg editor

--- editor.js
+++ editor.js
@@ -1,9 +1,43 @@
+const ThemeNamespace = 'yournamespace'
+
+/**
+ * Remove styles from blocks
+ */
+wp.hooks.addFilter(
+  'blocks.registerBlockType',
+  `${ThemeNamespace}/editor`,
+  function (settings, name) {
+    switch (name) {
+      // Image bock
+      case 'core/file': {
+        settings.attributes.showDownloadButton.default = false // Hide download button from file links
+        break
+      }
+      case 'core/video': {
+        settings.attributes.loop.default = true
+        settings.attributes.playsInline.default = true
+        settings.attributes.muted.default = true
+        settings.attributes.autoplay.default = true
+        settings.attributes.controls.default = false
+      }
+
+      case 'core/image': {
+        settings.styles = []
+      }
+    }
+
+    return settings
+  }
+)
+
 wp.domReady(function () {
   /**
    * Remove editor panels
    */
   wp.data.dispatch('core/editor').removeEditorPanel('taxonomy-panel-post_tag')
   wp.data.dispatch('core/editor').removeEditorPanel('page-attributes')
+  wp.data.dispatch('core/edit-post').removeEditorPanel('post-excerpt')
+  wp.data.dispatch('core/edit-post').removeEditorPanel('advanced')
 
   /**
    * Remove rich text formats from rich text blocks.
@@ -20,25 +54,6 @@ wp.domReady(function () {
   wp.richText.unregisterFormatType('core/text-color')
   wp.richText.unregisterFormatType('core/underline')
 })
-
-/**
- * Remove styles from blocks
- */
-wp.hooks.addFilter(
-  'blocks.registerBlockType',
-  'YOUR_NAMESPACE/editor',
-  function (settings, name) {
-    switch (name) {
-      // Image bock
-      case 'core/file': {
-        settings.attributes.showDownloadButton.default = false // Hide download button from file links
-        break
-      }
-    }
-
-    return settings
-  }
-)
 
 /**
  * Unregister plugins
@@ -65,4 +80,17 @@ wp.domReady(function () {
       wp.blocks.unregisterBlockVariation('core/embed', blockVariation.name)
     }
   })
+})
+
+/**
+ * Disable fullscreen mode
+ */
+wp.domReady(function () {
+  const isFullscreenMode = wp.data
+    .select('core/edit-post')
+    .isFeatureActive('fullscreenMode')
+
+  if (isFullscreenMode) {
+    wp.data.dispatch('core/edit-post').toggleFeature('fullscreenMode')
+  }
 })
