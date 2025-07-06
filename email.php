fb790a311bf59627a06dc70951bc1b817a8c1c28
Vincent Orback <hello@vincentorback.se>
2025-07-06 19:02:43 +0200

    Remove admin email verification screen

--- email.php
+++ email.php
@@ -38,3 +38,12 @@ add_filter(
  * Disable sending email change confirmation emails
  */
 add_filter( 'send_email_change_email', '__return_false' );
+
+
+
+/**
+ * Remove admin email verification screen
+ *
+ * @link https://developer.wordpress.org/reference/hooks/admin_email_check_interval/
+ */
+add_filter( 'admin_email_check_interval', '__return_false' );
