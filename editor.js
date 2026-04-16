9544477454dc8f9b009ac4f891b0fac7459db3d8
Vincent Orback <hello@vincentorback.se>
2026-04-16 16:19:08 +0200

    unregister stretchy paragraph and heading blocks

--- editor.js
+++ editor.js
@@ -40,6 +40,12 @@ wp.domReady(function () {
   wp.data.dispatch('core/edit-post').removeEditorPanel('advanced')
 
   /**
+   * Remove stretchy blocks
+   */
+  wp.blocks.unregisterBlockVariation('core/paragraph', 'stretchy-paragraph')
+  wp.blocks.unregisterBlockVariation('core/heading', 'stretchy-heading')
+
+  /**
    * Remove rich text formats from rich text blocks.
    */
   wp.richText.unregisterFormatType('core/bold')
@@ -54,6 +60,18 @@ wp.domReady(function () {
   wp.richText.unregisterFormatType('core/superscript')
   wp.richText.unregisterFormatType('core/text-color')
   wp.richText.unregisterFormatType('core/underline')
+
+  // Unregister all embed blocks
+  wp.blocks.getBlockVariations('core/embed').forEach(function (blockVariation) {
+    wp.blocks.unregisterBlockVariation('core/embed', blockVariation.name)
+  })
+  // ...or unregister all embeds but the ones you want
+  const allowedEmbedBlocks = ['vimeo', 'youtube']
+  wp.blocks.getBlockVariations('core/embed').forEach(function (blockVariation) {
+    if (!allowedEmbedBlocks.includes(blockVariation.name)) {
+      wp.blocks.unregisterBlockVariation('core/embed', blockVariation.name)
+    }
+  })
 })
 
 /**
@@ -80,18 +98,4 @@ wp.domReady(function () {
   wp.plugins.unregisterPlugin('jetpack-sidebar')
   wp.plugins.unregisterPlugin('jetpack-social-previews')
   wp.plugins.unregisterPlugin('jetpack-likes-and-sharing-panel')
-
-  // Unregister all embed blocks
-  wp.blocks.getBlockVariations('core/embed').forEach(function (blockVariation) {
-    wp.blocks.unregisterBlockVariation('core/embed', blockVariation.name)
-  })
-
-  // Unregister all embeds but the ones you want
-  const allowedEmbedBlocks = ['vimeo', 'youtube']
-
-  wp.blocks.getBlockVariations('core/embed').forEach(function (blockVariation) {
-    if (!allowedEmbedBlocks.includes(blockVariation.name)) {
-      wp.blocks.unregisterBlockVariation('core/embed', blockVariation.name)
-    }
-  })
 })
